b41183bb8fdf96a1a2988342c68da0cabb955c99
D'Arcy Rittich <drittich@gmail.com>
2023-12-03 08:29:35 -0500

    prepend chunk header

--- SemanticSlicer/ISlicer.cs
+++ SemanticSlicer/ISlicer.cs
@@ -6,6 +6,6 @@ namespace SemanticSlicer
 {
 	public interface ISlicer
 	{
-		List<DocumentChunk> GetDocumentChunks(string content, Dictionary<string, object?>? metadata = null);
+		List<DocumentChunk> GetDocumentChunks(string content, Dictionary<string, object?>? metadata = null, string chunkHeader = "");
 	}
 }
